1b6981346ab30df78e7f6ba6ec82b90ecd130b04
Clayton Jerlow <clayton.jerlow@hotmail.com>
2018-03-12 14:02:42 -0700

    updated smooth surf

--- brainsuite/brainsuite.py
+++ brainsuite/brainsuite.py
@@ -1684,7 +1684,7 @@ class SVRegSmoothSurfFunction(CommandLine):
     _cmd = 'svreg_smooth_surf_function.sh'
     
     def _gen_filename(self, name):
-        return outSurface
+        return self.inputs.outSurface
     
     def _list_outputs(self):
         return l_outputs(self)
