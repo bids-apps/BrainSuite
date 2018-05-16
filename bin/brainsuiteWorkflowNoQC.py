a00efa6dd1b3415ed0abd1311b26c716ad29d14f
Yeun Kim <yeunkim@gmail.com>
2018-05-16 08:02:17 -0700

    Bug fix 1

--- bin/brainsuiteWorkflowNoQC.py
+++ bin/brainsuiteWorkflowNoQC.py
@@ -230,6 +230,9 @@ def runWorkflow(SUBJECT_ID, INPUT_MRI_FILE, WORKFLOW_BASE_DIRECTORY, BIDS_DIRECT
         applyMapFRT_GFAObj.inputs.outFile = applyMapInputBase + '.dwi.RAS.correct.atlas.FRT_GFA.nii.gz'
         applyMapFRT_GFAObj.inputs.targetFile = applyMapTargetFile
 
+        ds2 = pe.Node(io.DataSink(), name='DATASINK2')
+        ds2.inputs.base_directory = WORKFLOW_BASE_DIRECTORY
+
         brainsuite_workflow.connect(ds2, 'out_file', applyMapFAObj, 'dataSinkDelay')
         brainsuite_workflow.connect(ds2, 'out_file', applyMapMDObj, 'dataSinkDelay')
         brainsuite_workflow.connect(ds2, 'out_file', applyMapAxialObj, 'dataSinkDelay')
